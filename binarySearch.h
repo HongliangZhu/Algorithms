4c4b4932dfb8c810882dabf74477650cfd6f43ad
HongliangZhu <hlzhumail@gmail.com>
2014-10-09 23:54:22 +0800

    commit

--- binarySearch.h
+++ binarySearch.h
@@ -1,24 +1,123 @@
 #pragma once
 
-int binarySearch(int * s, int len, int key)
+int binarySearch(int A[], int n, int x)
 {
-    //binary search consider len==0
-    //while quick sort consider len<=0
-    if(len==0)
-        return -1;
-
-    int half = len/2;
-
-    if(s[half]==key)
+    int low = 0, high=n-1, mid;
+    while(low<=high)
     {
-        return half;
+        mid = (low+high)/2;
+        if (x==A[mid])
+        {
+            return mid;
+        }
+        if (x<A[mid])
+        {
+            high = mid - 1;
+        } 
+        else
+        {
+            low = mid + 1;
+        }
     }
-    else if(key < s[half])
+
+    return -1;
+}
+
+int binarySearch(int A[], int n, int x, bool searchFirst)
+{
+    int low = 0, high=n-1, mid, result=-1;
+    while(low<=high)
     {
-        binarySearch(s, half, key);
+        mid = (low+high)/2;
+        if (x==A[mid])
+        {
+            result = mid;
+            if (searchFirst) high = mid - 1;
+            else low = mid + 1;
+        }
+        if (x<A[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+
+    return result;
+}
+
+int countOccurence(int A[], int n, int x)
+{
+    int firstIndex = binarySearch(A, n, x, true);
+    if(firstIndex==-1)
+    {
+        return 0;
     }
     else
     {
-        binarySearch(s+half+1, len-half-1, key);
+        int lastIndex = binarySearch(A, n, x, false);
+        return lastIndex-firstIndex+1;
     }
 }
+
+//number of rotate is the index of the minimum number in A.
+//case 1: A[low] < A[high] 
+//return low;
+//
+//next = (mid+1)%N
+//prev = (mid+N-1)%N
+//case 2: A.mid <A.next && A.mid <A.prev
+//return mid
+//
+//case 3: A.mid<A.high
+//high = (mid+N-1)%N;
+//case 4: A.mid>A.low
+//low = (mid+1)%N;
+int FindRotationCount(int A[], int n)
+{
+    int low = 0, high = n-1;
+
+    while(low<=high)
+    {
+        if (A[low]<=A[high])
+            return low;
+
+        int mid = (low+high)/2;
+        int next = (mid+1)%n;
+        int prev = (mid+n-1)%n;
+        if (A[mid]<=A[next] && A[mid]<=A[prev])
+            return mid;
+
+        if (A[mid]<=A[high])
+            high = mid-1;
+        else//if (A[mid]>A[low])
+            low = mid+1;
+    }
+}
+
+int binarySearchInRotatedArray(int A[], int n, int x)
+{
+    int low = 0, high = n - 1;
+    while(low<=high)
+    {
+        int mid = (low+high)/2;
+        if(x==A[mid])
+            return mid;
+
+        if (A[low]<A[mid])
+        {
+            if (x>=A[low] && x<A[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        else
+        {
+            if (x>A[mid] && x<=A[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
